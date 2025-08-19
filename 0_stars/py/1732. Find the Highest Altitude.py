2d3f8520cf14cc5216aebb696c21bdb58b7cf42e
Ge Zhang <gz19950616@gmail.com>
2025-08-19 19:51:38 -0400

    add 1732

---
+++ 0_stars/py/1732. Find the Highest Altitude.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def largestAltitude(self, gain):
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+        ans = 0
+        cur = 0
+        for i in gain:
+            cur += i
+            ans = max(cur, ans)
+        return ans
