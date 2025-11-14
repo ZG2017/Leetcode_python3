c327c4688b0c88fd64f0906f6efb15d49735d149
Ge Zhang <gz19950616@gmail.com>
2025-11-14 01:35:17 -0500

    update 338

--- 0_stars/py/338. Counting Bits.py
+++ 0_stars/py/338. Counting Bits.py
@@ -14,7 +14,7 @@ class Solution:
         return dp
 
 
-# updated: (bit computation)
+# updated: (bit computation + dp)
 class Solution(object):
     def countBits(self, num):
         """
@@ -25,4 +25,18 @@ class Solution(object):
         for x in range(1, num + 1):
             ans += ans[x >> 1] + (x & 1),
         return ans
+
+
+# pure bit computation
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+        for i in range(n+1):
+            tmp = 0
+            while i > 0:
+                tmp += 1
+                i = i & (i-1)
+            res.append(tmp)
+        return res
+        
 
