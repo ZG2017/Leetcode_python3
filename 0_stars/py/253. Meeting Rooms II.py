d296ca204eead7c9f83ad3c7a03d0c3e23fece75
Ge Zhang <gz19950616@gmail.com>
2025-12-20 21:24:42 -0500

    update 253

--- 0_stars/py/253. Meeting Rooms II.py
+++ 0_stars/py/253. Meeting Rooms II.py
@@ -1,4 +1,4 @@
-# 根据所有时间排序，计算最多重合的会议数，（重复的时间则结束在前，开始在后）
+# sorted events and count the maximum number of concurrent events
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         events = [(iv[0], 1) for iv in intervals] + [(iv[1], -1) for iv in intervals]
@@ -10,4 +10,26 @@ class Solution:
             ans = max(ans, cur)
         return ans
 
+
+# priority queue version
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        import heapq
+        q = []
+        for interval in intervals:
+            start, end = interval
+            q.append((start, 1))
+            q.append((end, 0))
+        heapq.heapify(q)
+        c = 0
+        max_c = 0
+        while q:
+            cur = heapq.heappop(q)
+            if cur[1] == 1:
+                c += 1
+                max_c = max(c, max_c)
+            else:
+                c -= 1
+        return max_c
 
