9057b9d18509bd01048b51586c021612375fd8d9
Ge Zhang <gz19950616@gmail.com>
2026-04-25 01:10:04 -0400

    add 364

---
+++ 1_stars/py/364. Nested List Weight Sum II.py
@@ -0,0 +1,66 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def __init__(self):
+        self.maxdepth = 0
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        if len(nestedList) == 0:
+            return 0 
+        self.finddepth(nestedList, 1)
+        return self.findSum(nestedList, 1)
+
+    def finddepth(self, l, currdepth):
+        self.maxdepth = max(self.maxdepth, currdepth)
+        for i in l:
+            if not i.isInteger():
+                self.finddepth(i.getList(), currdepth+1)
+    
+    def findSum(self, l, currdepth):
+        sum = 0
+        for i in l:
+            if not i.isInteger():
+                sum+=self.findSum(i.getList(), currdepth+1)
+            else:
+                sum+=(self.maxdepth - currdepth + 1) * i.getInteger()
+        return sum
